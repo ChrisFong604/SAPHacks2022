04a1de61a84adb8b709604aedd8351db0725ab62
ChrisFong604 <ccffoonngg@gmail.com>
2022-10-02 11:20:52 -0700

    added horizontal card component in communities page

--- src/pages/app/events/index.js
+++ src/pages/app/events/index.js
@@ -1,5 +1,27 @@
 import React from 'react'
 
+/* const upcomingEvents = [
+  { name: 'Board games night', date: 'Thurs Oct 6, 7:00 - 9:00p.m' },
+  { name: 'Pride Gathering', date: 'All day' },
+  { name: 'Board games night', date: 'Thurs Oct 6, 7:00 - 9:00p.m' },
+  { name: 'Board games night', date: 'Thurs Oct 6, 7:00 - 9:00p.m' },
+  { name: 'Board games night', date: 'Thurs Oct 6, 7:00 - 9:00p.m' },
+  { name: 'Board games night', date: 'Thurs Oct 6, 7:00 - 9:00p.m' },
+  { name: 'Board games night', date: 'Thurs Oct 6, 7:00 - 9:00p.m' },
+  { name: 'Board games night', date: 'Thurs Oct 6, 7:00 - 9:00p.m' },
+  { name: 'Board games night', date: 'Thurs Oct 6, 7:00 - 9:00p.m' },
+  { name: 'Board games night', date: 'Thurs Oct 6, 7:00 - 9:00p.m' },
+  { name: 'Board games night', date: 'Thurs Oct 6, 7:00 - 9:00p.m' },
+  { name: 'Board games night', date: 'Thurs Oct 6, 7:00 - 9:00p.m' },
+]
+const HorizontalCard = ({ community }) => (
+  <div>
+    <div>{community.name}</div>
+    <div>{community.location}</div>
+    <div>{community.description}</div>
+  </div>
+) */
+
 export default function Events() {
   return <div>Events Page</div>
 }
