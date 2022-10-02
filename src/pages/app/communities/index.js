987f37591fba915145a1c75aa1cad9217e455656
Matt Wong <wongmatthew357@gmail.com>
2022-10-02 10:18:22 -0700

    update communities page

--- src/pages/app/communities/index.js
+++ src/pages/app/communities/index.js
@@ -1,13 +1,11 @@
-import { useSelector } from 'react-redux'
 import styles from './communities.module.scss'
 
 export default function Communities() {
-  const { me } = useSelector((state) => state.app)
 
   return (
     <>
       <div style={styles.root}>
-        <div className={styles.main_grid}>Communities page + {me}</div>
+        <div className={styles.main_grid}>Communities page</div>
       </div>
     </>
   )
